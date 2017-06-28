3eef51feb3e7beff3ab0703dd08e761dd305ad50
amparish <alexmparish@gmail.com>
2017-06-28 02:09:57 -0500

    basic structure written

---
+++ ClozeCard.js
@@ -0,0 +1,29 @@
+function ClozeCard(text, cloze){
+	this.text = text,
+	this.cloze = cloze
+}
+
+ClozeCard.prototype.partial = function(){
+
+}
+
+ClozeCard.prototype.fullText = function(){
+	
+}
+
+var firstPresidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+
+// "George Washington"
+console.log(firstPresidentCloze.cloze); 
+
+// " ... was the first president of the United States."
+console.log(firstPresidentCloze.partial);
+
+// "George Washington was the first president of the United States."
+console.log(firstPresidentCloze.fullText);
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
+
+module.exports = ClozeCard;
